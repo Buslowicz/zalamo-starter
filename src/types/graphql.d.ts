b57411b95868f8193bd48bdd0ffd54566a2b8ff4
Daniel Busłowicz <draccoz@gmail.com>
2017-02-21 16:27:03 +0100

    Added dependencies

---
+++ src/types/graphql.d.ts
@@ -0,0 +1,97 @@
+
+export interface Query {
+  posts: Array<Post> | null;
+  post: Post | null;
+  authors: Array<Author> | null;
+  votes: Array<Post> | null;
+}
+
+export interface PostQueryArgs {
+  postId: number | null;
+}
+
+export interface Post {
+  id: number;
+  title: string | null;
+  author: Author | null;
+  votes: number | null;
+}
+
+export interface Author {
+  id: number;
+  firstName: string | null;
+  lastName: string | null;
+  posts: Array<Post> | null;
+}
+
+export interface Mutation {
+  upvotePost: Post | null;
+}
+
+export interface UpvotePostMutationArgs {
+  postId: number;
+}
+
+export interface Subscription {
+  postUpvoted: Post | null;
+}
+
+export namespace AllPostsQuery {
+  export type Variables = {
+  }
+
+  export type Result = {
+    posts: Array<Posts>;
+  } 
+
+  export type Posts = {
+    id: number;
+    title: string;
+    votes: number;
+    author: Author;
+  } 
+
+  export type Author = {
+    id: number;
+    firstName: string;
+    lastName: string;
+  } 
+}
+
+export namespace GetPostQuery {
+  export type Variables = {
+      postId: number;
+  }
+
+  export type Result = {
+    post: Post;
+  } 
+
+  export type Post = {
+    id: number;
+    title: string;
+    votes: number;
+    author: Author;
+  } 
+
+  export type Author = {
+    id: number;
+    firstName: string;
+    lastName: string;
+  } 
+}
+
+export namespace UpvotePostMutation {
+  export type Variables = {
+      postId: number;
+  }
+
+  export type Result = {
+    upvotePost: UpvotePost;
+  } 
+
+  export type UpvotePost = {
+    id: number;
+    votes: number;
+  } 
+}
---
+++ src/types/index.d.ts
@@ -0,0 +1,34 @@
+import { Post } from './graphql';
+import { ApolloQueryObservable, Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { ApolloQueryResult } from 'apollo-client';
+
+export interface AppState {
+  counter?: number;
+  posts?: Array<Post>;
+}
+
+export type ApolloQuery<T> = ApolloQueryObservable<T>;
+export type ApolloMutation<T> = Observable<ApolloQueryResult<T>>;
+
+export interface Cast<V> extends Apollo {
+  watchQuery(data: {query: any, variables?: V}): ApolloQueryObservable<any>;
+  query(data: {query: any, variables?: V}): ApolloQueryObservable<any>;
+  mutate(data: {mutation: any, variables?: V}): Observable<ApolloQueryResult<any>>;
+}
+
+declare enum ApolloEvent {
+  APOLLO_QUERY_RESULT,
+  APOLLO_QUERY_ERROR,
+  APOLLO_QUERY_INIT,
+  APOLLO_QUERY_RESULT_CLIENT,
+  APOLLO_QUERY_STOP,
+  APOLLO_MUTATION_INIT,
+  APOLLO_MUTATION_RESULT,
+  APOLLO_MUTATION_ERROR,
+  APOLLO_UPDATE_QUERY_RESULT,
+  APOLLO_STORE_RESET,
+  APOLLO_SUBSCRIPTION_RESULT,
+}
+
+export * from './graphql';
